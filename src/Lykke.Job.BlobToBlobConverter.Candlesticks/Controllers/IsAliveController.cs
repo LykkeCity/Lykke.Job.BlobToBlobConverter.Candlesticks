88e8470e6d8e7dad1693e0b19d68476a2e6a2999
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-11-16 15:21:44 +0300

    Nuget updates

--- src/Lykke.Job.BlobToBlobConverter.Candlesticks/Controllers/IsAliveController.cs
+++ src/Lykke.Job.BlobToBlobConverter.Candlesticks/Controllers/IsAliveController.cs
@@ -4,7 +4,7 @@ using Lykke.Common;
 using Lykke.Common.Api.Contract.Responses;
 using Lykke.Job.BlobToBlobConverter.Candlesticks.Core.Services;
 using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Lykke.Job.BlobToBlobConverter.Candlesticks.Controllers
 {
